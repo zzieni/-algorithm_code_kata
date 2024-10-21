d5d63be59fea95faffe99f09942a202d3173ced0
Kim Ji Eun <{>
2024-10-21 09:30:34 +0900

    3_두수의 몫

---
+++ 03.js
@@ -0,0 +1,10 @@
+/**
+ * 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(num1, num2) {
+  return parseInt(num1 / num2);
+}
+
+console.log(solution(10, 5));
+console.log(solution(7, 2));
