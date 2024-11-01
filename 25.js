ac2b29528ae435cf44e09c740be307e942baa18e
Kim Ji Eun <{>
2024-11-01 09:44:20 +0900

    25_나누어 떨어지는 숫자 배열

---
+++ 25.js
@@ -0,0 +1,16 @@
+/**
+ * array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+ * divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+ */
+
+function solution(arr, divisor) {
+  let result = arr.filter((i) => {
+    return i % divisor === 0;
+  });
+
+  return result.length === 0 ? [-1] : result.sort((a, b) => a - b);
+}
+
+console.log(solution([5, 9, 7, 10], 5));
+console.log(solution([2, 36, 1, 3], 1));
+console.log(solution([3, 2, 6], 10));
