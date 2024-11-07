c143310dae98ebe66b9c39543a7d221344978edc
Kim Ji Eun <{>
2024-11-08 08:49:00 +0900

    31_수박수박수박수박수박수?

---
+++ 31.js
@@ -0,0 +1,10 @@
+/**
+ * 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+ * 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+ */
+
+function solution(n) {
+  return '수박'.repeat(n).substring(0, n);
+}
+console.log(solution(3));
+console.log(solution(4));
